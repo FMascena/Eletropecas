78c3d820546d87d72cbade9e78444eead5746275
fmascena <felipemasscena16@gmail.com>
2023-08-31 22:48:21 -0300

    update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LogoFooter from '../../assets/Sub logo principal.png';
-import LogoFB from "../../assets/face.png"
-import LogoInsta from "../../assets/insta.png"
+import LogoFooter from '../../assets/Sub logo principal (1).png';
+import LogoFB from "../../assets/face-removebg-preview.png"
+import LogoInsta from "../../assets/insta-removebg-preview.png"
 import './index.css';
 
 function Footer() {
