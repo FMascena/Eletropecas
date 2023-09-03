557126a129cec180687dccd7ec69f224a3c658e9
fmascena <felipemasscena16@gmail.com>
2023-09-02 21:56:41 -0300

    feature partners and avaliations

--- src/components/Avaliations/index.tsx
+++ src/components/Avaliations/index.tsx
@@ -1,12 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './index.css';
 
 function Avaliations() {
+    const testimonialsData = [
+        {
+            name: 'Felipe Ferreira',
+            company: 'Auto Peças Felipe',
+            message:
+                'Sou cliente há 3 anos, excelente atendimento, equipe fera. E sempre trazendo produtos de qualidade e com preço justo.',
+        },
+        {
+            name: 'Jane Smith',
+            company: 'XYZ Creative',
+            message:
+                'Pellentesque tincidunt libero id eros facilisis, in suscipit libero luctus. Nullam fringilla odio et elit malesuada dapibus.',
+        },
+        {
+            name: 'Bob Johnson',
+            company: '123 Tech',
+            message:
+                'Sed vestibulum elit eu libero fermentum, eget tincidunt urna fermentum. Nunc laoreet nibh vel lectus scelerisque tincidunt.',
+        },
+    ];
+
     return (
         <div className="container-avaliations">
             <div className='container-background'>
-                <h1 className='titulo-avaliations'>Avaliações</h1>
+                <h1 className='titulo-avaliations'>Depoimentos</h1>
+            </div>
+            <div className="testimonials-container">
+                <Carousel showArrows={true}>
+                    {testimonialsData.map((testimonial, index) => (
+                        <div key={index} className="testimonial">
+                            <h3>{testimonial.name}</h3>
+                            <p>{testimonial.company}</p>
+                            <p>{testimonial.message}</p>
+                        </div>
+                    ))}
+                </Carousel>
             </div>
         </div>
     );
--- src/components/Partners/index.tsx
+++ src/components/Partners/index.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './index.css';
+import { Carousel } from 'react-responsive-carousel';
+import Logo1 from "../../assets/gbusch-logo.jpg"
+import Logo2 from "../../assets/Hella_logo.png"
+import Logo3 from "../../assets/hikari-logo.webp"
 
 function Partners() {
     return (
         <div className="container-partners">
             <div className='container-background'>
                 <h1 className='titulo-partners'>Parceiros</h1>
+            </div>
+            <div className="carousel-partners">
+                <h1>Conheça alguns de nossos parceiros</h1>
+                <Carousel showArrows={true}>
+                    <div className='container-logos'>
+                        <img src={Logo1} alt="Imagem 1" className='logo-partners' />
+                    </div>
+                    <div className='container-logos'>
+                        <img src={Logo2} alt="Imagem 2" className='logo-partners' />
+                    </div>
+                    <div className='container-logos'>
+                        <img src={Logo3} alt="Imagem 3" className='logo-partners'/>
+                    </div>
+                </Carousel>
             </div>
         </div>
     );
