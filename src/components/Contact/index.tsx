bcd5c9490f2ed66e6dff350ba2c7d08ac9db0386
fmascena <felipemasscena16@gmail.com>
2023-09-04 00:21:14 -0300

    add .env

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './index.css';
 
 const Contact = () => {
+
   return (
     <div className="contact-page">
       <div className="map-container">
---
+++ src/server.js
@@ -0,0 +1,19 @@
+// server.js
+// const express = require('express');
+// const dotenv = require('dotenv');
+
+// dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.get('/api/chave-api', (req, res) => {
+//   const minhaChave = process.env.MINHA_CHAVE_DE_API;
+//   res.json({ chave: minhaChave });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta ${PORT}`);
+// });
+
+// Este é um comentário de exemplo, você pode descomentar as linhas conforme necessário
