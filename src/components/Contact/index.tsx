d70894a993927f5ec61e905cfde3f91847e37936
fmascena <felipemasscena16@gmail.com>
2023-09-04 13:08:34 -0300

    fix contact build bug

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -8,10 +8,9 @@ const Contact = () => {
         <iframe
           title="Localização da Empresa"
           src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAZDMSDCawk_lKI4fIXbBxpVwgJ8fSKO3E&q=Eletropeças, Rua+General+Lima+da+Silva,+85+-+Aerolândia,+Fortaleza+-+CE,+60850-430`}
-          width="100%"
-          height="400"
-          frameBorder="0"
-          allowFullScreen=""
+          width={100}
+          height={400}
+          frameBorder={0}
         ></iframe>
       </div>
       <div className="contact-form">
@@ -26,13 +25,13 @@ const Contact = () => {
             </div>
           </div>
           <div className="form-group">
-              <input type="tel" id="phone" name="phone" placeholder="Telefone" />
-            </div>
+            <input type="tel" id="phone" name="phone" placeholder="Telefone" />
+          </div>
           <div className="form-group">
             <input type="text" id="subject" name="subject" placeholder="Assunto" />
           </div>
           <div className="form-group">
-            <textarea id="message" name="message" rows="4" placeholder="Mensagem"></textarea>
+            <textarea id="message" name="message" rows={4} placeholder="Mensagem"></textarea>
           </div>
           <button type="submit">Enviar mensagem</button>
         </form>
