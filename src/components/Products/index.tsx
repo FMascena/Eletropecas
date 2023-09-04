ccfab264cc8a8e993112757cf5e830daad63ca31
fmascena <felipemasscena16@gmail.com>
2023-09-03 22:10:25 -0300

    update products

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,49 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Alternadores from "../../assets/alternadores.png"
-import Ventiladores from "../../assets/ventiladores.png"
-import BobinaDeCampo from "../../assets/bobina-de-campo.png"
-import KitsReparo from "../../assets/kits-reparo.png"
-import Lampadas from "../../assets/lampadas.png"
-import FusivelLamina from "../../assets/fusivel-lamina.png"
-import ReguladorVoltagem from "../../assets/regulador-voltagem.png"
-import PortaEscovas from "../../assets/porta-escovas.png"
-import ConjuntoPlanetaria from "../../assets/conjunto-planetaria.png"
+import Eletrica from "../../assets/ELETRICA.png"
+import Interruptores from "../../assets/Interruptores-1.png"
+import Fitas from "../../assets/FITAS-1.png"
+import Suspensao from "../../assets/SUSPENSAO.png"
+import Quimicos from "../../assets/QUIMICOS.png"
+import Iluminacao from "../../assets/ILUMINAÇÃO-1.png"
+import Conectores from "../../assets/CONECTORES-1.png"
+import Cabos from "../../assets/CABOS-1.png"
+import Fusíveis from "../../assets/FUSIVEIS-1.png"
 import './index.css';
 
 function Products() {
     return (
         <div className="container-products">
             <div className='products-background'>
-                <h1 className='titulo-produtos'>Produtos</h1>
+                <h1 className='titulo-produtos'>Categorias de Produtos</h1>
             </div>
             <div className='container-product'>
                 <div className='product-box'>
-                    <img src={Alternadores} alt="Alternadores" className='product' />
+                    <img src={Eletrica} alt="Alternadores" className='product' />
                 </div>
                 <div className='product-box'>
-                    <img src={Ventiladores} alt="Ventiladores" className='product' />
+                    <img src={Quimicos} alt="Ventiladores" className='product' />
                 </div>
                 <div className='product-box'>
-                    <img src={BobinaDeCampo} alt="Bobina de Campo" className='product' />
+                    <img src={Suspensao} alt="Bobina de Campo" className='product' />
                 </div>
                 <div className='product-box'>
-                    <img src={KitsReparo} alt="Kits Reparo" className='product' />
+                    <img src={Fitas} alt="Kits Reparo" className='product' />
                 </div>
                 <div className='product-box'>
-                    <img src={Lampadas} alt="Lâmpadas" className='product' />
+                    <img src={Interruptores} alt="Lâmpadas" className='product' />
                 </div>
                 <div className='product-box'>
-                    <img src={FusivelLamina} alt="Fusível Lâmina" className='product' />
+                    <img src={Iluminacao} alt="Fusível Lâmina" className='product' />
                 </div>
                 <div className='product-box'>
-                    <img src={ReguladorVoltagem} alt="Regulador de Voltagem" className='product' />
+                    <img src={Conectores} alt="Regulador de Voltagem" className='product' />
                 </div>
                 <div className='product-box'>
-                    <img src={PortaEscovas} alt="Porta Escovas" className='product' />
+                    <img src={Cabos} alt="Porta Escovas" className='product' />
                 </div>
                 <div className='product-box'>
-                    <img src={ConjuntoPlanetaria} alt="Conjunto Planetaria" className='product' />
+                    <img src={Fusíveis} alt="Conjunto Planetaria" className='product' />
                 </div>
             </div>
         </div>
